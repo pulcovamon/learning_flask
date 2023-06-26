17481f271a160452b8739dd4c4348f88e102c9ba
Monika <pulcovamon@gmail.com>
2023-06-26 23:39:54 +0200

    validations

--- app03/market/routes.py
+++ app03/market/routes.py
@@ -1,6 +1,6 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app, db
+from flask import render_template, redirect, url_for, flash
+from market.models import Item, User
 from market.forms import RegisterForm
 
 @app.route('/')
@@ -13,7 +13,18 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    print(form.errors)
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}')
     return render_template('register.html', form=form)
